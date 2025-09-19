be9cd434e0c630a9ef4796d7f105121796e46aca
Lorenzo Fenderico <lorenzoFenderico.dev@gmail.com>
2025-09-19 08:52:14 +0200

    v2 scraping api (cleaner data)

--- backend/main.py
+++ backend/main.py
@@ -10,13 +10,26 @@ def scrapeUrl(url:str):
     scrapedWebpage = BeautifulSoup(webpage.content, "html.parser")
     
     title = scrapedWebpage.find("title").text
-    headingsContent = [heading.get_text() for heading in scrapedWebpage.find_all("h1", "h2")]
+    headingsContent = [heading.get_text() for heading in scrapedWebpage.find_all(["h1", "h2"])]
     paragraphContent = [p.get_text() for p in scrapedWebpage.find_all("p")]
+    divContent = [div.get_text() for div in scrapedWebpage.find_all("div")]
+
+    def cleanContent(contentList):
+        cleanedList = []
+        for content in contentList:
+            if len(content) > 10:
+                cleanedList.append(content)
+        return [' '.join(content.strip().split()) for content in cleanedList]
+    
+    clearedHeadingsContent = cleanContent(headingsContent)
+    clearedParagraphContent = cleanContent(paragraphContent)
+    clearedDivContent = cleanContent(divContent)
 
     result = {
         "Title": title,
-        "Headings": headingsContent,
-        "Paragraphs": paragraphContent
+        "Headings": clearedHeadingsContent,
+        "Paragraphs": clearedParagraphContent,
+        "Div": clearedDivContent
     }
 
     return result
