188d17d43cca3abb3a81560cd74d5842c3fb6089
Lorenzo Fenderico <lorenzoFenderico.dev@gmail.com>
2025-10-10 22:03:22 +0200

    Added navigation menu

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,32 @@
 <template>
-    <div class="mx-[10%]">
-        <div class="bg-neutral-100 rounded-full p-4 w-fit font-bold mx-auto my-4 text-xl">
-            NEUROCLIP
+    <div class="mx-[10%] mb-20">
+        <div class="bg-neutral-100 rounded-full px-4 py-2  w-fit mx-auto my-4">
+            <div class="flex flex-row gap-2 items-center justify-center">
+                <NuxtLink to="/"  class="font-bold text-xl h-full">NEUROCLIP</NuxtLink>
+                <button @click="menu = !menu" :class="{'bg-neutral-200' : menu == true}"   class="text-4xl h-full flex items-center rounded-full p-2"><Icon name="ic:round-menu"></Icon></button>
+            </div>
+
+            <transition
+            enter-active-class="transition-all duration-300 ease-out"
+            leave-active-class="transition-all duration-200 ease-in"
+            enter-from-class="opacity-0 -translate-y-4 scale-95"
+            enter-to-class="opacity-100 translate-y-0 scale-100"
+            leave-from-class="opacity-100 translate-y-0 scale-100"
+            leave-to-class="opacity-0 -translate-y-4 scale-95"
+            >
+                <div v-if="menu == true" class="absolute top-24 left-1/2 transform -translate-x-1/2 mt-2 bg-neutral-100/80 backdrop-blur-md rounded-3xl z-50 w-48 h-fit p-8 flex flex-col gap-6">
+                    <div class="flex flex-col gap-4 items-center justify-center text-lg">
+                        <NuxtLink to="/Help" class="hover:opacity-50 transition-opacity">Help</NuxtLink>
+                        <NuxtLink to="/Pricing" class="hover:opacity-50 transition-opacity">Pricing</NuxtLink>
+                        <NuxtLink to="/About" class="hover:opacity-50 transition-opacity">About us</NuxtLink>
+                        <NuxtLink to="/FAQ" class="hover:opacity-50 transition-opacity">FAQ</NuxtLink>
+                    </div>
+                    <div class="flex flex-col gap-4 items-center justify-center">
+                        <button class="button bg-white shadow-lg shadow-neutral-900 hover:opacity-75 transition-opacity">LogIn</button>
+                        <button class="button bg-neutral-900 text-white shadow-lg shadow-neutral-900 hover:opacity-90 transition-opacity">SignUp</button>
+                    </div>
+                </div>
+            </transition>
         </div>
 
         <div class="h-fit mt-14">
@@ -9,7 +34,7 @@
             <p class="paragraph text-center my-6 mx-2">From long websites to short summary - Use AI to optimize your workflow</p>
 
             <div class="flex flex-col gap-4 justify-center items-center">
-                <NuxtLink to="/app" class="button text-white bg-black text-xl">Start now</NuxtLink>
+                <NuxtLink to="/app" class="button text-white bg-neutral-900 text-xl">Start now</NuxtLink>
                 <button class="button text-lg">Play Demo</button>
             </div>
 
@@ -117,7 +142,7 @@
                 </div>
 
                 <div class="flex flex-col gap-4 items-start">
-                    <NuxtLink to="/app" class="button text-white bg-black text-xl">Start now</NuxtLink>
+                    <NuxtLink to="/app" class="button text-white bg-neutral-900 text-xl">Start now</NuxtLink>
                 <button class="button text-lg">Play Demo</button>
             </div>
             </div>
@@ -125,10 +150,8 @@
     </div>
 </template>
 
+
+
 <script setup lang="ts">
-
+const menu = ref<boolean>(false)
 </script>
-
-<style scoped>
-
-</style>
