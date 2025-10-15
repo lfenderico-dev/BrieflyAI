61245a449044956ffe6710a0b2e392ff3c93cfec
Lorenzo Fenderico <lorenzoFenderico.dev@gmail.com>
2025-10-15 22:36:23 +0200

    Update explaination.vue

--- frontend/pages/explaination.vue
+++ frontend/pages/explaination.vue
@@ -10,10 +10,10 @@
             <p class="paragraph text-center">BrieflyAI takes an URL, scrapes every content of the page and lets AI analyze it. After this AI provides to us a summary of the page with the most important informations in a simpler way, cleaning out any of the techical and boring content and replacing it with explainations that even a 5 years old boy can understand. Beside this BrieflyAI also has its own chatbot you can use to ask questions about the content you found, make notes and even exames simulations, exercises and more.</p>   
         </div>
 
-        <div>
+        <div class="flex flex-col gap-4">
             <h1 class="header1">Are you in?</h1>
             <p class="paragraph text-center">Join now to get demo access!</p>
-            <NuxtLink class="button flex justify-center items-center" to="/authentication">Sign In</NuxtLink>
+            <NuxtLink class="button flex justify-center items-center bg-neutral-900 text-white text-xl" to="/authentication">Sign In</NuxtLink>
         </div>
     </div>
 </template>
